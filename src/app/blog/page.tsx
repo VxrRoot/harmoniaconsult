a0f4e0d896466a464aa49110d4073540775d8fcc
Dawid Slowik <dawidslowik379@gmail.com>
2024-02-07 08:29:27 +0100

    temporary revalidate

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,8 @@ import Frame from "@/ui/atoms/frame";
 import { getPosts } from "@/lib/query";
 import blogImg from "../../assets/single-blog-post.svg";
 
-export const revalidate = 86400;
+// export const revalidate = 86400;
+export const revalidate = 1200;
 
 const BlogPage = async () => {
   const data: ISimplyBlogCard[] = await getPosts();
